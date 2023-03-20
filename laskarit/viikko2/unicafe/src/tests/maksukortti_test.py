40e2af5c38041140262aeab6ab33c279ddf7dfb4
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-03-20 18:31:57 +0200

    test: complete week2 task6:
    'Takaisin testeihin'

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_luodun_kortin_saldo_on_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_lataa_rahaa_kasvattaa_kortin_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(2000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 30.00 euroa")
+
+    def test_ota_rahaa_vahentaa_saldoa_oikein_jos_saldoa_on_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(750)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 2.50 euroa")
+
+    def test_ota_rahaa_ei_muuta_saldoa_jos_saldo_ei_riita(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_ota_rahaa_palauttaa_True_jos_rahat_riittivat(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(750), True)
+
+    def test_ota_rahaa_palauttaa_False_jos_rahat_eivat_riittaneet(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1500), False)
