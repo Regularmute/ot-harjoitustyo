57d3c21363a1641fae969597dc3e43c11127db8d
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-03-20 19:29:41 +0200

    test: complete tests for week2 task8
    Add tests for card payments with enough saldo and too little saldo.
    Add tests to check charging money on the card works correctly.
    Initialize a payment card with little money to avoid redundancy with
    testing card payments without enough money.

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -73,3 +73,83 @@ class TestKassapaate(unittest.TestCase):
         self.assertEqual(self.kassapaate.maukkaat, maukkaat_myynnit_alussa)
 
 # Korttiostot
+# Onnistuneet ostokset, kun kortilla on tarpeeksi rahaa
+    def test_syo_edullisesti_kortilla_vahentaa_kortin_saldoa_oikein(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
+
+        self.assertEqual(self.maksukortti.saldo, 760)
+
+    def test_syo_edullisesti_kortilla_palauttaa_true_jos_kortilla_on_tarpeeksi_rahaa(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(self.maksukortti), True)
+
+    def test_syo_edullisesti_kortilla_nostaa_edullisten_myynnit_oikein(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
+
+        self.assertEqual(self.kassapaate.edulliset, 1)
+
+    def test_syo_edullisesti_kortilla_ei_muuta_kassan_rahamaaraa(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_syo_maukkaasti_kortilla_vahentaa_kortin_saldoa_oikein(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
+
+        self.assertEqual(self.maksukortti.saldo, 600)
+
+    def test_syo_maukkaasti_kortilla_palauttaa_true_jos_kortilla_on_tarpeeksi_rahaa(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti), True)
+
+    def test_syo_maukkaasti_kortilla_nostaa_edullisten_myynnit_oikein(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
+
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_syo_maukkaasti_kortilla_ei_muuta_kassan_rahamaaraa(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+# Epäonnistuneet ostokset, kun kortilla ei ole tarpeeksi rahaa
+
+    def test_syo_edullisesti_kortilla_ei_vahenna_kortin_saldoa_jos_saldo_ei_riita(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.koyha_kortti)
+
+        self.assertEqual(self.koyha_kortti.saldo, 20)
+
+    def test_syo_edullisesti_kortilla_palauttaa_false_jos_kortilla_ei_ole_tarpeeksi_rahaa(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(self.koyha_kortti), False)
+
+    def test_syo_edullisesti_kortilla_ei_nosta_edullisten_myynteja_jos_saldo_ei_riita(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.koyha_kortti)
+
+        self.assertEqual(self.kassapaate.edulliset, 0)
+
+    def test_syo_edullisesti_kortilla_ei_muuta_kassan_rahamaaraa_vaikka_saldo_ei_riita(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.koyha_kortti)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_syo_maukkaasti_kortilla_ei_vahenna_kortin_saldoa_jos_saldo_ei_riita(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.koyha_kortti)
+
+        self.assertEqual(self.koyha_kortti.saldo, 20)
+
+    def test_syo_maukkaasti_kortilla_palauttaa_false_jos_kortilla_ei_ole_tarpeeksi_rahaa(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kortilla(self.koyha_kortti), False)
+
+    def test_syo_maukkaasti_kortilla_ei_nosta_maukkaiden_myynteja_jos_saldo_ei_riita(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.koyha_kortti)
+
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_syo_maukkaasti_kortilla_ei_muuta_kassan_rahamaaraa_vaikka_saldo_ei_riita(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.koyha_kortti)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+# Kortin saldon lataus
+
+    def test_lataa_rahaa_kortille_nostaa_kortin_saldoa_oikein(self):
+        self.kassapaate.lataa_rahaa_kortille(self.maksukortti, 2000)
+        self.assertEqual(self.maksukortti.saldo, 3000)
+
+    def test_lataa_rahaa_kortille_nostaa_kassan_rahamaaraa_oikein(self):
+        self.kassapaate.lataa_rahaa_kortille(self.maksukortti, 2000)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 102000)
