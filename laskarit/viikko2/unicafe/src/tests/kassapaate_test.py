516bcf59cc2760904cae6dacb14669fe927e51b5
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-03-20 18:52:30 +0200

    test: set up kassapaate for tests and create a test

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -2,4 +2,8 @@ import unittest
 from kassapaate import Kassapaate
 
 class TestKassapaate(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.kassapaate = Kassapaate()
+
+    def test_luodun_kassapaatteen_rahat_on_oikein(self):
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
