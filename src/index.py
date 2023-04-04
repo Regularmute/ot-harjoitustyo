d261eefc64712232834fbd95f4bf72c655602000
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-04-04 12:21:08 +0300

    Refactor: modularize view files

--- src/index.py
+++ src/index.py
@@ -1,31 +1,20 @@
-from tkinter import Tk, ttk, constants
+from tkinter import Tk
+from login_view import LoginView
 
 class UI:
     def __init__(self, root):
         self._root = root
+        self._current_view = None
 
     def start(self):
-        heading_label = ttk.Label(master=self._root, text="Pathfinder 2E Character Sheet")
+        self._show_login_view()
 
-        username_label = ttk.Label(master=self._root, text="Username")
-        username_entry = ttk.Entry(master=self._root)
+    def _show_login_view(self):
+        self._current_view = LoginView(
+            self._root
+        )
 
-
-        password_label = ttk.Label(master=self._root, text="Password")
-        password_entry = ttk.Entry(master=self._root)
-
-        login_button = ttk.Button(master=self._root, text="Login")
-        register_button = ttk.Button(master=self._root, text="Register")
-
-        heading_label.grid(row=0, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
-        username_label.grid(row=1, column=0, padx=5, pady=5)
-        username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
-        password_label.grid(row=2, column=0, padx=5, pady=5)
-        password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
-        login_button.grid(row=3, column=0, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
-        register_button.grid(row=4, column=0, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
-
-        self._root.grid_columnconfigure(1, weight=1, minsize=300)
+        self._current_view.pack()
 
 window = Tk()
 window.title("Pathdinder 2e Character Sheet")
---
+++ src/login_view.py
@@ -0,0 +1,37 @@
+from tkinter import ttk, constants
+
+class LoginView:
+    def __init__(self, root):
+        self._root = root
+        self._frame = None
+
+        self._initialize()
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+
+    def destroy(self):
+        self._frame.destroy()
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+        heading_label = ttk.Label(master=self._frame, text="Pathfinder 2E Character Sheet")
+
+        username_label = ttk.Label(master=self._frame, text="Username")
+        username_entry = ttk.Entry(master=self._frame)
+
+        password_label = ttk.Label(master=self._frame, text="Password")
+        password_entry = ttk.Entry(master=self._frame)
+
+        login_button = ttk.Button(master=self._frame, text="Login")
+        register_button = ttk.Button(master=self._frame, text="Register")
+
+        heading_label.grid(row=0, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
+        username_label.grid(row=1, column=0, padx=5, pady=5)
+        username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        password_label.grid(row=2, column=0, padx=5, pady=5)
+        password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        login_button.grid(row=3, column=0, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
+        register_button.grid(row=4, column=0, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
+
+        self._root.grid_columnconfigure(1, weight=1, minsize=300)
