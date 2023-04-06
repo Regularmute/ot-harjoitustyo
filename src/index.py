b89ac4eca544a106dbe55e1b065f92cd4cb05f59
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-04-06 17:40:43 +0300

    refactor(structure): modularize ui components

--- src/index.py
+++ src/index.py
@@ -1,52 +1,5 @@
 from tkinter import Tk
-from login_view import LoginView
-from register_view import RegisterView
-from sheet_view import SheetView
-
-class UI:
-    def __init__(self, root):
-        self._root = root
-        self._current_view = None
-
-    def start(self):
-        self._show_login_view()
-
-    def _hide_current_view(self):
-        if self._current_view:
-            self._current_view.destroy()
-
-        self._current_view = None
-
-    def _show_login_view(self):
-        self._hide_current_view()
-
-        self._current_view = LoginView(
-            self._root,
-            self._show_register_view,
-            self._show_sheet
-        )
-
-        self._current_view.pack()
-
-    def _show_register_view(self):
-        self._hide_current_view()
-
-        self._current_view = RegisterView(
-            self._root,
-            self._show_login_view
-        )
-
-        self._current_view.pack()
-
-    def _show_sheet(self):
-        self._hide_current_view()
-
-        self._current_view = SheetView(
-            self._root,
-            self._show_login_view
-        )
-
-        self._current_view.pack()
+from ui.ui import UI
 
 window = Tk()
 window.title("Pathfinder 2e Character Sheet")
---
+++ src/ui/ui.py
@@ -0,0 +1,48 @@
+from ui.login_view import LoginView
+from ui.register_view import RegisterView
+from ui.sheet_view import SheetView
+
+class UI:
+    def __init__(self, root):
+        self._root = root
+        self._current_view = None
+
+    def start(self):
+        self._show_login_view()
+
+    def _hide_current_view(self):
+        if self._current_view:
+            self._current_view.destroy()
+
+        self._current_view = None
+
+    def _show_login_view(self):
+        self._hide_current_view()
+
+        self._current_view = LoginView(
+            self._root,
+            self._show_register_view,
+            self._show_sheet
+        )
+
+        self._current_view.pack()
+
+    def _show_register_view(self):
+        self._hide_current_view()
+
+        self._current_view = RegisterView(
+            self._root,
+            self._show_login_view
+        )
+
+        self._current_view.pack()
+
+    def _show_sheet(self):
+        self._hide_current_view()
+
+        self._current_view = SheetView(
+            self._root,
+            self._show_login_view
+        )
+
+        self._current_view.pack()
