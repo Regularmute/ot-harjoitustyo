4c01ca40d931157684cf739e267fe4015a9dd8b2
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-04-16 18:24:15 +0300

    refactor(entities): property names to snake_case

--- src/entities/character.py
+++ src/entities/character.py
@@ -1,7 +1,7 @@
 class Character:
-    def __init__(self, name, level, xp, hp, prof_bonus):
+    def __init__(self, name, level, experience, hit_points, prof_bonus):
         self.name = name
         self.level = level
-        self.xp = xp
-        self.hp = hp
+        self.experience = experience
+        self.hit_points = hit_points
         self.prof_bonus = prof_bonus
