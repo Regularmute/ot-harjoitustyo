230ba3613abb7de0d0dc6143bcc26eb9dd723ef7
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-04-17 10:34:38 +0300

    test(user-repo): create tests for current methods

--- src/tests/user_repository_test.py
+++ src/tests/user_repository_test.py
@@ -15,3 +15,30 @@ class TestUserRepository(unittest.TestCase):
         self.assertEqual(len(users), 1)
         self.assertEqual(users[0].username, self.user_jaana.username)
         self.assertEqual(users[0].password, self.user_jaana.password)
+
+    def test_get_all_returns_user_objects_correctly(self):
+        user_repository.create(self.user_jaana)
+        user_repository.create(self.user_keijo)
+        users = user_repository.get_all()
+
+        self.assertEqual(len(users), 2)
+        self.assertEqual(users[0].username, self.user_jaana.username)
+        self.assertEqual(users[0].password, self.user_jaana.password)
+        self.assertEqual(users[1].username, self.user_keijo.username)
+        self.assertEqual(users[1].password, self.user_keijo.password)
+
+    def test_get_one_by_username_returns_correct_user(self):
+        user_repository.create(self.user_jaana)
+        user_repository.create(self.user_keijo)
+        fetched_user = user_repository.get_one_by_username("keijokoo")
+
+        self.assertEqual(fetched_user.username, self.user_keijo.username)
+        self.assertEqual(fetched_user.password, self.user_keijo.password)
+
+    def test_delete_all_empties_table_correctly(self):
+        user_repository.create(self.user_jaana)
+        user_repository.create(self.user_keijo)
+
+        user_repository.delete_all()
+        users = user_repository.get_all()
+        self.assertEqual(len(users), 0)
