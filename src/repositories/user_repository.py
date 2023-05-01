44fc9067e5eaac4c67fb4a64b3c589b4e004f22a
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-05-01 16:34:08 +0300

    style(user-repo): split lines to stay under 79 char

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,20 +1,27 @@
-"""Käyttäjätietokannan käsittelylogiikka."""
+"""Tietokannan käyttäjätaulukon käsittelylogiikka."""
 
 from database_connection import get_database_connection
 from entities.user import User
 
 
 def _get_user_by_row(row):
-    """Luo käyttäjä-olion SQLitestä haetusta rivistä. Helpottaa muun sovelluksen toimintaa.
+    """Luo käyttäjä-olion SQLitestä haetusta rivistä. Helpottaa muun
+    sovelluksen toimintaa.
 
     Args:
-        row: Halutun käyttäjän rivi käyttäjätaulukosta. Funktio olettaa, että rivi sisältää sarakkeet "username", "password" ja "user_id".
+        row: Halutun käyttäjän rivi käyttäjätaulukosta. Funktio olettaa, että
+        rivi sisältää sarakkeet "username", "password" ja "user_id".
 
     Returns:
-        Käyttäjä-olio, jolle annetaan parametreiksi taulukkorivin sarakkeiden arvot. None jos funktio kutsuttiin ilman riviä.
+        Käyttäjä-olio, jolle annetaan parametreiksi taulukkorivin sarakkeiden
+        arvot. None jos funktio kutsuttiin ilman riviä.
     """
 
-    return User(row["username"], row["password"], row["user_id"]) if row else None
+    return User(
+            row["username"],
+            row["password"],
+            row["user_id"]
+        ) if row else None
 
 
 class UserRepository:
@@ -22,10 +29,12 @@ class UserRepository:
         self._connection = connection
 
     def get_all(self):
-        """Toteuttaa SQL-kyselyn yhdistettyyn tietokantaan, joka palauttaa listan kaikista käyttäjistä.
+        """Toteuttaa SQL-kyselyn yhdistettyyn tietokantaan, joka palauttaa
+        listan kaikista käyttäjistä.
 
         Returns:
-            Lista käyttäjä-olioista, jotka on luotu suorittamalla _get_user_by_row jokaiseen kyselyn palauttamaan riviin.
+            Lista käyttäjä-olioista, jotka on luotu suorittamalla
+            _get_user_by_row jokaiseen kyselyn palauttamaan riviin.
         """
 
         cursor = self._connection.cursor()
@@ -36,35 +45,41 @@ class UserRepository:
         return list(map(_get_user_by_row, rows))
 
     def get_one_by_username(self, username):
-        """Toteuttaa SQL-kyselyn yhdistettyyn tietokantaan, joka palauttaa käyttäjän, jolla on tietty käyttäjätunnus.
+        """Toteuttaa SQL-kyselyn yhdistettyyn tietokantaan, joka palauttaa
+        käyttäjän, jolla on tietty käyttäjätunnus.
 
         Args:
             username (str): Haetun käyttäjän käyttäjätunnus.
 
         Returns:
-            Käyttäjä-olio, joka on luotu suorittamalla _get_user_by_row kyselyn palauttamaan riviin.
+            Käyttäjä-olio, joka on luotu suorittamalla _get_user_by_row
+            kyselyn palauttamaan riviin.
         """
 
         cursor = self._connection.cursor()
 
-        sql = "SELECT user_id, username, password FROM users WHERE username = :username"
+        sql = """SELECT user_id, username, password FROM users
+                 WHERE username = :username"""
         cursor.execute(sql, {"username": username})
         row = cursor.fetchone()
 
         return _get_user_by_row(row)
 
     def create(self, user):
-        """Toteuttaa SQL-kyselyn yhdistettyyn tietokantaan, joka tallettaa käyttäjän taulukkoon.
+        """Toteuttaa SQL-kyselyn yhdistettyyn tietokantaan, joka tallettaa
+        käyttäjän taulukkoon.
 
         Args:
             user (User): Käyttäjäolio, joka talletetaan SQLite-tietokantaan.
 
         Returns:
-            Talletettu käyttäjäolio. Oliolta puuttuu SQLitessä sille asetettu hahmotunniste.
+            Talletettu käyttäjäolio. Oliolta puuttuu SQLitessä sille asetettu
+            tunnisteluku.
         """
 
         cursor = self._connection.cursor()
-        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        sql = """INSERT INTO users (username, password)
+                 VALUES (:username, :password)"""
 
         cursor.execute(
             sql, {"username": user.username, "password": user.password})
@@ -74,7 +89,8 @@ class UserRepository:
         return user
 
     def delete_all(self):
-        """Toteuttaa SQL-kyselyn yhdistettyyn tietokantaan, joka poistaa kaikki käyttäjätaulukon käyttäjät."""
+        """Toteuttaa SQL-kyselyn yhdistettyyn tietokantaan, joka poistaa
+        kaikki käyttäjätaulukon käyttäjät."""
 
         cursor = self._connection.cursor()
 
