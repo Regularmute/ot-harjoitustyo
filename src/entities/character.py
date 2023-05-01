cebe3cff2e1eb5553923c4a21a586b8ecf4cf9c4
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-05-01 14:24:10 +0300

    docs(entities): create docstring

--- src/entities/character.py
+++ src/entities/character.py
@@ -1,5 +1,20 @@
 class Character:
+    """Hahmo-olio jonka avulla käsitellään taulukkoihin meneviä ja sieltä tulevia rivejä.
+
+    Attributes:
+        creator_id (int): Hahmon luoneen käyttäjän tunnisteluku.
+        name (str): Hahmon nimi.
+        level (int): Hahmon taso.
+        experience (int): Hahmon kokemuspisteet.
+        hit_points (int): Hahmon vahinkopisteet.
+        character_id (int): Hahmon oma tunnisteluku. Asetetaan SQLitessä.
+    """
+
     def __init__(self, creator_id, name, level, experience, hit_points, character_id=None):
+        """Alustaa olion annetuilla parametreilla. Tunnisteluku on None, kunnes se haetaan
+            hahmotaulukosta, jossa se on asetettu.
+        """
+
         self.creator_id = creator_id
         self.name = name
         self.level = level
--- src/entities/user.py
+++ src/entities/user.py
@@ -1,6 +1,17 @@
 class User:
+    """Käyttäjä-olio jonka avulla käsitellään taulukkoihin meneviä ja sieltä tulevia rivejä.
+
+        Attributes:
+            username (str): Käyttäjän tunnus.
+            password (str): Käyttäjän salasana. Hashattu sovelluslogiikan puolella.
+            user_id (str): Käyttäjän identifioiva tunnistenumero. Asetetaan SQLitessä.
+    """
 
     def __init__(self, username, passwordhash, user_id=None):
+        """Alustaa olion annetuilla parametreillä. User_id on tyhjä,
+            kunnes se haetaan taulukosta jossa se on asetettu.
+        """
+
         self.username = username
         self.password = passwordhash
         self.user_id = user_id
