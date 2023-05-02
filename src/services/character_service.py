3acfcf702dd86c1b759e623a48c3a885c24261fa
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-05-02 18:34:17 +0300

    docs(char-serv): add docstring

--- src/services/character_service.py
+++ src/services/character_service.py
@@ -1,3 +1,8 @@
+"""Hahmo-olioiden sovelluslogiikka.
+
+Kutsuu character_repository.py:tä tietokannan käsittelemistä varten.
+"""
+
 from repositories.character_repository import (
     character_repository as default_character_repository
 )
@@ -19,22 +24,70 @@ class CharacterService:
         return character
 
     def get_characters(self):
+        """Hakee tietokannasta kaikki hahmot.
+
+        Returns:
+            Lista jokaisen tietokannan rivin tiedoista muodostetuista
+            hahmoolioista.
+        """
+
         return self._character_repository.get_all()
 
     def get_character_by_creator_id(self, creator_id):
+        """VANHENTUNUT: Hakee tietokannasta tietyn käyttäjän ylimmän hahmon.
+
+        Args:
+            creator_id(int): Hahmon luoneen käyttäjän tunnisteluku.
+
+        Returns:
+            Käyttäjän luoma hahmo-olio joka on ylimpänä tietokannassa.
+        """
         return self._character_repository.get_one_by_creator_id(creator_id)
 
     def get_all_by_creator_id(self, creator_id):
+        """Hakee tietokannasta kaikki tietyn käyttäjän luomat hahmot.
+
+        Args:
+            creator_id(int): Hahmojen luoneen käyttäjän tunnisteluku.
+
+        Returns:
+            Lista hahmo-olioista jotka parametrin käyttäjä on luonut.
+        """
+
         return self._character_repository.get_all_by_creator_id(creator_id)
 
-    def get_character_by_character_id(self, character_id):
-        return self._character_repository.get_one_by_character_id(character_id)
+    def get_character_by_character_id(self, char_id):
+        """Hakee tietokannasta tietyn hahmon tunnisteluvun perusteella.
+
+        Args:
+            char_id(int): Haettavan hahmon tunnisteluku.
+
+        Returns:
+            Hahmo-olio, jolla on vastaava tunnisteluku.
+        """
+
+        return self._character_repository.get_one_by_character_id(char_id)
 
     def set_character_name(self, character_id, new_name):
-        return self._character_repository.update_character_name(character_id, new_name)
+        """Muokkaa hahmon nimeä tietokannassa.
+
+        Args:
+            character_id(int): Muokattavan hahmon tunnisteluku.
+            new_name(str): Uusi nimi joka annetaan hahmolle.
+        """
+        self._character_repository.update_character_name(
+            character_id, new_name)
 
     def set_character_statistic(self, character_id, statistic, new_value):
-        return self._character_repository.update_character_property(
+        """Muokkaa jotakin hahmon ominaisuutta tietokannassa.
+
+        Args:
+            character_id(int): Muokattavan hahmon tunnisteluku.
+            statistic(str): Ominaisuus, jota halutaan muokata.
+            new_value(int): Uusi arvo muokattavalle ominaisuudelle.
+        """
+
+        self._character_repository.update_character_property(
             character_id, statistic, new_value)
 
 
