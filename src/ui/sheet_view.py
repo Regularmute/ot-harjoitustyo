41a6c1d3e9156f1efe170a036ade3ca72a909e6c
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-05-02 20:13:45 +0300

    feat(sheet): user can edit exp and hp

--- src/ui/sheet_view.py
+++ src/ui/sheet_view.py
@@ -109,6 +109,86 @@ class SheetView:
         self._edit_level_btn.grid(
             row=3, column=5, padx=5, pady=5)
 
+    def _edit_experience_handler(self):
+        self._edit_experience_btn.grid_remove()
+        self._experience_entry.grid(
+            row=4, column=1, padx=5, pady=5)
+        self._confirm_experience_btn.grid(
+            row=4, column=2, padx=5, pady=5)
+
+    def _edit_experience_confirm_handler(self):
+        new_experience = self._experience_entry.get()
+        character_service.set_character_statistic(
+            self._character.character_id, "experience", new_experience)
+        self._character = character_service.get_character_by_character_id(
+            self._character.character_id)
+
+        self._experience_label.grid_remove()
+        self._character_experience_label.grid_remove()
+        self._experience_entry.grid_remove()
+        self._confirm_experience_btn.grid_remove()
+
+        self._initialize_experience_field()
+
+    def _initialize_experience_field(self):
+        self._experience_label = ttk.Label(
+            master=self._frame, text="Experience:")
+        self._character_experience_label = ttk.Label(
+            master=self._frame, text=f"{self._character.experience}"
+        )
+        self._experience_entry = ttk.Entry(master=self._frame)
+        self._edit_experience_btn = ttk.Button(
+            master=self._frame, text="Edit", command=self._edit_experience_handler)
+        self._confirm_experience_btn = ttk.Button(
+            master=self._frame, text="Confirm", command=self._edit_experience_confirm_handler)
+
+        self._experience_label.grid(
+            row=4, column=0, sticky=constants.W, padx=5, pady=5)
+        self._character_experience_label.grid(
+            row=4, column=1, sticky=constants.W, padx=5, pady=5)
+        self._edit_experience_btn.grid(
+            row=4, column=2, padx=5, pady=5)
+
+    def _edit_hit_points_handler(self):
+        self._edit_hit_points_btn.grid_remove()
+        self._hit_points_entry.grid(
+            row=4, column=4, padx=5, pady=5)
+        self._confirm_hit_points_btn.grid(
+            row=4, column=5, padx=5, pady=5)
+
+    def _edit_hit_points_confirm_handler(self):
+        new_hit_points = self._hit_points_entry.get()
+        character_service.set_character_statistic(
+            self._character.character_id, "hit_points", new_hit_points)
+        self._character = character_service.get_character_by_character_id(
+            self._character.character_id)
+
+        self._hit_points_label.grid_remove()
+        self._character_hit_points_label.grid_remove()
+        self._hit_points_entry.grid_remove()
+        self._confirm_hit_points_btn.grid_remove()
+
+        self._initialize_hit_points_field()
+
+    def _initialize_hit_points_field(self):
+        self._hit_points_label = ttk.Label(
+            master=self._frame, text="Hit points:")
+        self._character_hit_points_label = ttk.Label(
+            master=self._frame, text=f"{self._character.hit_points}"
+        )
+        self._hit_points_entry = ttk.Entry(master=self._frame)
+        self._edit_hit_points_btn = ttk.Button(
+            master=self._frame, text="Edit", command=self._edit_hit_points_handler)
+        self._confirm_hit_points_btn = ttk.Button(
+            master=self._frame, text="Confirm", command=self._edit_hit_points_confirm_handler)
+
+        self._hit_points_label.grid(
+            row=4, column=3, sticky=constants.W, padx=5, pady=5)
+        self._character_hit_points_label.grid(
+            row=4, column=4, sticky=constants.W, padx=5, pady=5)
+        self._edit_hit_points_btn.grid(
+            row=4, column=5, padx=5, pady=5)
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
         heading_label = ttk.Label(
@@ -120,11 +200,13 @@ class SheetView:
 
         self._initialize_name_field()
         self._initialize_level_field()
+        self._initialize_experience_field()
+        self._initialize_hit_points_field()
 
         heading_label.grid(row=0, column=0, columnspan=2,
                            sticky=constants.W, padx=5, pady=5)
         username_label.grid(row=1, column=0, columnspan=3, padx=5, pady=5)
-        return_button.grid(row=4, column=0, columnspan=2,
+        return_button.grid(row=5, column=0, columnspan=2,
                            sticky=(constants.EW), padx=5, pady=5)
 
         self._root.grid_columnconfigure(1, weight=1, minsize=300)
